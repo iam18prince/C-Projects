1058583519e5c3ed665672bea4aba51a06fa44c8
Prince Kumar <kumar4149krishna@gmail.com>
2026-09-22 20:54:41 +0530

    Add files via upload

---
+++ Number Square.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+     int n;
+     printf("ENTER NUMBER:-");
+     scanf("%d",&n);
+     for (int i=1; i<=n;i++){
+     for (int i=1;i<=n;i++){
+     printf("%d",i);
+     }
+     printf("\n");
+     }
+     
+return 0;
+}
